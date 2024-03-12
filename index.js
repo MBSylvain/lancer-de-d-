1d5fb9a771969a07632389ab4be34d61ce81f450
MBSylvain <155894547+MBSylvain@users.noreply.github.com>
2024-03-12 06:51:19 +0100

    lancer de dé et alterance des joeur

--- index.js
+++ index.js
@@ -10,20 +10,49 @@ function lancerDe() {
     console.log(resultatD);
     //Affiche la valeur du dé
     document.getElementById("resultatDe").innerHTML = (resultatD);
-    var jeu = 0;
-    jeu += ++jeu + jeu++;
+
     //nombre de tour
     tr = tr + 1;
     const bouton = document.getElementById('LancerD');
-    bouton.addEventListener('click', tour);
+    bouton.addEventListener('click', lancerDe);
     console.log(tr)
     //alternance des lanceurs
     if (tr % 2 === 0) {
         console.log("c'est au joueur n°01 de jouer");
+        function mjscore() {
+            let a = (document.getElementById('resultatDe'));
+            // Récupérer le texte affiché dans l'élément + convertion en nombre
+            var texteAffiche = Number(a.textContent);
+            global += texteAffiche;
+            console.log("Score global :", global);
+
+            // Vérifier si le score global atteint ou dépasse 100
+            if (global >= 30) {
+                console.log("Félicitations ! Vous avez gagné !");
+                // Vous pouvez ajouter d'autres actions ici, comme afficher un message de victoire dans l'interface utilisateur.
+            } else {
+                console.log("Non encore gagné. Continuez à jouer !");
+            }
+        };
     } else {
         console.log("c'est au joueur n°02 de jouer")
+        function mjscore() {
+            let b = (document.getElementById('resultatDe'));
+            // Récupérer le texte affiché dans l'élément + convertion en nombre
+            var texteAffiche = Number(a.textContent);
+            global2 += texteAffiche;
+            console.log("Score global :", global2);
+
+            // Vérifier si le score global atteint ou dépasse 100
+            if (global2 >= 30) {
+                console.log("Félicitations ! Vous avez gagné !");
+                // Vous pouvez ajouter d'autres actions ici, comme afficher un message de victoire dans l'interface utilisateur.
+            } else {
+                console.log("Non encore gagné. Continuez à jouer !");
+            }
+        };
     }
-}
+};
 
 
 // Mettre à jour le score global en ajoutant le résultat du dé
@@ -45,6 +74,3 @@ function mjscore() {
     }
 };
 
-function tour() {
-
-} 
